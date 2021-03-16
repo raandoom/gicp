1d7cdce099ec996b0dea9b24c6bdc0a341d01dfa
Sergey Savkin <raandoom@users.noreply.github.com>
2021-03-17 00:51:58 +0300

    Indentation and spaces instead of tabs

--- scan.h
+++ scan.h
@@ -2,15 +2,15 @@
   Generalized-ICP Copyright (c) 2009 Aleksandr Segal.
   All rights reserved.
 
-  Redistribution and use in source and binary forms, with 
-  or without modification, are permitted provided that the 
+  Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that the
   following conditions are met:
 
-* Redistributions of source code must retain the above 
-  copyright notice, this list of conditions and the 
+* Redistributions of source code must retain the above
+  copyright notice, this list of conditions and the
   following disclaimer.
 * Redistributions in binary form must reproduce the above
-  copyright notice, this list of conditions and the 
+  copyright notice, this list of conditions and the
   following disclaimer in the documentation and/or other
   materials provided with the distribution.
 * The names of the contributors may not be used to endorse
@@ -22,13 +22,13 @@
   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
   DAMAGE.
@@ -42,37 +42,37 @@
 #include <vector>
 
 typedef struct {
-  double mat[3][3]; // 36 bytes, row major
-  int transpose;   // 4
-  int symmetric;   // 4
-  int diagonal;    // 4
-} dgc_matrix3d_t;  // = 48 bytes per matrix      
+    double mat[3][3]; // 36 bytes, row major
+    int transpose;   // 4
+    int symmetric;   // 4
+    int diagonal;    // 4
+} dgc_matrix3d_t;  // = 48 bytes per matrix
 
 typedef struct {
-  double vec[3];    // 12 bytes
-} dgc_vector3d_c_t;  // 16 bytes per vector                                                                                                                                                                       
+    double vec[3];    // 12 bytes
+} dgc_vector3d_c_t;  // 16 bytes per vector
 
 typedef struct {
-  double vec[3];    // 12 bytes
+    double vec[3];    // 12 bytes
 } dgc_vector3d_r_t;  // 16 bytes per vector
 
 typedef struct {
-  double                    x;
-  double                    y;
-  double                    z;
-  double                    roll;
-  double                    pitch;
-  double                    yaw;
+    double                    x;
+    double                    y;
+    double                    z;
+    double                    roll;
+    double                    pitch;
+    double                    yaw;
 } dgc_pose_t, * dgc_pose_p;
 
 class dgc_scan_t {
- public:
-  std::vector<dgc_vector3d_c_t> points;
-  std::vector<dgc_vector3d_c_t> norms;
-  dgc_pose_t pose;
-  
-  int save(const char* filename);
-  int load(const char *filename);
+public:
+    std::vector<dgc_vector3d_c_t> points;
+    std::vector<dgc_vector3d_c_t> norms;
+    dgc_pose_t pose;
+
+    int save(const char* filename);
+    int load(const char *filename);
 };
 
 
